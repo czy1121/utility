a8abaa80b750324821e5665cbef6c81ed1ebb8e1
ezy <249736246@qq.com>
2025-01-07 14:39:22 +0800

    #

--- utility/src/main/java/me/reezy/cosmo/utility/extension/coroutine.kt
+++ utility/src/main/java/me/reezy/cosmo/utility/extension/coroutine.kt
@@ -2,12 +2,6 @@ package me.reezy.cosmo.utility.extension
 
 import android.annotation.SuppressLint
 import android.widget.TextView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.lifecycle.withResumed
-import androidx.lifecycle.withStateAtLeast
 import kotlinx.coroutines.*
 
 
@@ -33,32 +27,4 @@ fun CoroutineScope.countdown(view: TextView, time: Long = 60, suffixText: String
         view.isEnabled = true
         view.text = doneText
     }
-}
-
-fun LifecycleOwner.loop(state: Lifecycle.State = Lifecycle.State.RESUMED, interval: Long = 1000, action: suspend () -> Unit) = lifecycleScope.launch {
-    repeatOnLifecycle(state) {
-        while (true) {
-            try {
-                action()
-            } catch (ex: Throwable) {
-                ex.printStackTrace()
-            }
-            delay(interval)
-        }
-    }
-}
-
-/**
- * 在 [Lifecycle.State] 每次进入 [state] 时运行 [block]
- * */
-fun LifecycleOwner.launchOn(state: Lifecycle.State = Lifecycle.State.RESUMED, block: suspend CoroutineScope.() -> Unit) = lifecycleScope.launch {
-    lifecycle.repeatOnLifecycle(state, block)
-}
-
-
-/**
- * 在 [Lifecycle.State] 至少为 [state] 的时运行 [block]
- * */
-fun LifecycleOwner.launchWith(state: Lifecycle.State = Lifecycle.State.RESUMED, block: () -> Unit) = lifecycleScope.launch {
-    lifecycle.withStateAtLeast(state, block)
 }
---
+++ utility/src/main/java/me/reezy/cosmo/utility/extension/lifecycle.kt
@@ -0,0 +1,39 @@
+package me.reezy.cosmo.utility.extension
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.withStateAtLeast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+fun LifecycleOwner.loop(state: Lifecycle.State = Lifecycle.State.RESUMED, interval: Long = 1000, block: suspend () -> Unit) = lifecycleScope.launch {
+    repeatOnLifecycle(state) {
+        while (true) {
+            try {
+                block()
+            } catch (ex: Throwable) {
+                ex.printStackTrace()
+            }
+            delay(interval)
+        }
+    }
+}
+
+/**
+ * 在 [Lifecycle.State] 每次进入 [state] 时运行 [block]
+ * */
+fun LifecycleOwner.repeatOn(state: Lifecycle.State = Lifecycle.State.RESUMED, block: suspend CoroutineScope.() -> Unit) = lifecycleScope.launch {
+    lifecycle.repeatOnLifecycle(state, block)
+}
+
+
+/**
+ * 在 [Lifecycle.State] 至少为 [state] 的时运行 [block]
+ * */
+fun LifecycleOwner.launchWith(state: Lifecycle.State = Lifecycle.State.RESUMED, block: () -> Unit) = lifecycleScope.launch {
+    lifecycle.withStateAtLeast(state, block)
+}
